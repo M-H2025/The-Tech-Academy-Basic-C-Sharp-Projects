d1b8e9c91a922205bcd58dbb480ab7c9cb3d49a4
M-H2025 <you.ho2024@gmail.com>
2026-03-07 11:32:34 -0500

    Add files via upload

---
+++ MethodAssignmentSubmission/MethodAssignmentSubmission/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    // This class contains a method that performs a math operation
+    public class MathOperations
+    {
+        // This method returns nothing and takes two integers as parameters
+        public void DoMath(int firstNumber, int secondNumber)
+        {
+            // Perform a math operation on the first number (multiply it by 2)
+            int result = firstNumber * 2;
+
+            // Display the result of the math operation to the console
+            Console.WriteLine("Result of math operation on first number: " + result);
+
+            // Display the second number to the screen 
+            Console.WriteLine("Second number received: " + secondNumber);
+        }
+    }
+}
---
+++ MethodAssignmentSubmission/MethodAssignmentSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations math = new MathOperations();
+
+            // Call the method using positional parameters
+            math.DoMath(5, 10);
+
+            // Call the method again using named parameters
+            math.DoMath(firstNumber: 7, secondNumber: 20);
+
+            // Pause the console so the user can see the output
+            Console.ReadLine();
+        }
+    }
+}
