960d0e3db23f21569c4b3a52bec2403cfae9a141
M-H2025 <you.ho2024@gmail.com>
2026-03-07 11:21:46 -0500

    Add files via upload

---
+++ MethodAssignment1/MethodAssignment1/MathOperation1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    public class MathOperation1
+    {
+        // Method that takes an integer and performs a math operation
+        public int DoMath(int number)
+        {
+            return number * 5;
+        }
+    }
+}
---
+++ MethodAssignment1/MethodAssignment1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOperation1 math = new MathOperation1();
+
+            // Ask the user for a number
+            Console.WriteLine("Enter a number:");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Call the method
+            int result = math.DoMath(userNumber);
+
+            // Print result
+            Console.WriteLine("The result is: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
